9ddb981df0251021fb147fb9e5c82404c2aebd8e
YeongsuKimm <yeongsuk505@gmail.com>
2023-10-04 15:37:32 -0700

    the developing procedure lecture

---
+++ variablesLecture.py
@@ -0,0 +1,30 @@
+num = 1;
+bool = True;
+stri = "hello";
+
+print(num)
+print(bool)
+print(stri)
+
+
+
+num1 = 1;
+num2 = 2;
+num3 = 3;
+
+
+#num1 ==> num3
+#num2 ==> num1
+#num3 ==> num2
+
+temp = num1;
+num1 = num3;
+temp2 = num2;
+num2 = temp;
+num3 = temp2;
+print(num1)
+print(num2)
+print(num3)
+
+lst = ["one","two","three"]
+print(lst[0])
